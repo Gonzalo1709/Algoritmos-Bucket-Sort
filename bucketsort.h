d1a03c9ef0f0b0a21e3f5b0762992d7bfdd4f142
Isabella Romero <isabella.romero@utec.edu.pe>
2023-08-21 16:54:50 -0500

    comments

--- bucketsort.h
+++ bucketsort.h
@@ -9,42 +9,34 @@
 
 using namespace std;
 
-// Algoritmo según https://www.geeksforgeeks.org/bucket-sort-2/:
-/* Create n empty buckets (Or lists) and do the following for every array element arr[i].
-
-    Insert arr[i] into bucket[n*array[i]]
-    Sort individual buckets using insertion sort.
-    Concatenate all sorted buckets.
- */
-
 void bucketSort(float arr[], int n) {
-    vector<float> bucketVector[n];
+    vector<float> bucketVector[n]; //creamos un vector de floats de tamaño n (cantidad de buckets)
 
-    for (int i = 0; i < n; i++) {
-        int bucket = n * arr[i];
-        bucketVector[bucket].push_back(arr[i]);
+    for (int i = 0; i < n; i++) { //inicializamos cada bucket con un vector vacío
+        int bucket = n * arr[i]; //seleccionamos el bucket correspondiente
+        bucketVector[bucket].push_back(arr[i]); //agregamos el elemento al bucket
     }
 
     // Se hizo el sorting concurrente para cada bucket
-    std::mutex mtx;
-    std::vector<std::thread> threads;
-    threads.reserve(n);
-    for (int i = 0; i < n; i++) {
-        threads.emplace_back([&bucketVector, &mtx, i]() {
-            std::lock_guard<std::mutex> lock(mtx);
-            std::sort(bucketVector[i].begin(), bucketVector[i].end());
-        });
+    std::mutex mtx; 
+    std::vector<std::thread> threads; //creamos un vector de hilos para cada bucket
+    threads.reserve(n); //reservamos espacio para n hilos
+    for (int i = 0; i < n; i++) { //inicializamos cada hilo
+        threads.emplace_back([&bucketVector, &mtx, i]() { //cada hilo ordena su bucket
+            std::lock_guard<std::mutex> lock(mtx); //se bloquea el mutex para que no se acceda a la misma variable
+            std::sort(bucketVector[i].begin(), bucketVector[i].end()); //se ordena el bucket
+        }); 
     }
-    for (auto &thread : threads) {
+    for (auto &thread : threads) { //se ejecuta cada hilo
         thread.join();
     }
 
-    int index = 0;
-    for (int i = 0; i < n; i++) {
-        for (float j : bucketVector[i]) {
-            arr[index++] = j;
+    int index = 0; //índice para recorrer el arreglo
+    for (int i = 0; i < n; i++) { //recorremos cada bucket
+        for (float j : bucketVector[i]) { //recorremos cada elemento del bucket
+            arr[index++] = j; //agregamos el elemento al arreglo
         }
-    }
+    } //al finalizar, el arreglo está ordenado
 }
 
 #endif //ALGORITMOS_Y_ESTRUCTURAS_DE_DATOS_BUCKETSORT_H
