ec14376cdd93b49b2a61bf7c2a6ffe9e2225cf98
Isabella Romero <isabella.romero@utec.edu.pe>
2023-08-22 21:05:41 -0500

    con archivo

--- main.cpp
+++ main.cpp
@@ -1,33 +1,44 @@
 #include <iostream>
 #include <random>
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+
 #include "bucketsort.h"
 
 using namespace std;
 
-int main() {
-    // Creando un array de floats aleatorios distribuidos uniformemente entre 0 y 1
-    int arraySize = 100;
+//create a .txt file to store the ordered and unordered arrays
+void createFile(string fileName) {
+    ofstream file;
+    int arraySize = 1000000;
     float arr[arraySize];
+    file.open(fileName);
+    int decimalPlaces = 2;
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(0.0, 1.0);
+    std::uniform_real_distribution<> dis(0.00, 1.00);
     for (int i = 0; i < arraySize; i++) {
         arr[i] = dis(gen);
     }
 
-    cout << "Array sin ordenar: " << endl;
+    file << "Array sin ordenar: " << endl;
     for (int i = 0; i < arraySize; i++) {
-        cout << arr[i] << " ";
+        file << (arr[i]) << " ";
     }
     cout << endl;
 
     bucketSort(arr, arraySize);
 
-    cout << "Array ordenada: " << endl;
+    file << "Array ordenada: " << endl;
     for (int i = 0; i < arraySize; i++) {
-        cout << arr[i] << " ";
+        file << (arr[i]) << " ";
     }
     cout << endl;
+}
 
-    return 0;
+int main() {
+    int arraySize = 1000000;
+    float arr[arraySize];
+    createFile("unorderedArray.txt");
 }
