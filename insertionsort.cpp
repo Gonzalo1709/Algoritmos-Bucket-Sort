02d74d82a0e17309032b1b44225e0a585b1d16e9
Isabella Romero <isabella.romero@utec.edu.pe>
2023-08-24 17:58:13 -0500

    insertion

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <random>
-#include <iomanip> // Necesario para std::setprecision
+#include <iomanip>
+#include <ctime>
 
 using namespace std;
 
-void insertionSort(float arr[], int n) {
+void insertionSort(double arr[], int n) {
     for (int i = 1; i < n; i++) {
-        int key = arr[i];
+        double key = arr[i];
         int j = i - 1;
 
         while (j >= 0 && arr[j] > key) {
@@ -18,26 +19,36 @@ void insertionSort(float arr[], int n) {
 }
 
 int main() {
-    int n = 1000;
-    float arr[n];
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(0.00, 1.00);
+    int n = 1000000;
+    double arr[n];
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> dis(0.00, 1.00);
+
     for (int i = 0; i < n; i++) {
         arr[i] = dis(gen);
     }
 
-    cout << "Array sin ordenar: " << endl;
+    cout << "Array sin ordenar:" << endl;
     for (int i = 0; i < n; i++) {
-        cout << std::fixed << std::setprecision(4) << arr[i] << " ";
+        cout << fixed << setprecision(4) << arr[i] << " ";
     }
     cout << endl;
+
+    clock_t start = clock();
+    start = clock();
 
     insertionSort(arr, n);
 
-    cout << "Array ordenada: " << endl;
+    cout << "Array ordenada:" << endl;
     for (int i = 0; i < n; i++) {
-        cout << std::fixed << std::setprecision(4) << arr[i] << " ";
+        cout << fixed << setprecision(4) << arr[i] << " ";
     }
     cout << endl;
+
+    clock_t end = clock();
+
+    cout << "Time elapsed: " << (double)(end - start) / CLOCKS_PER_SEC << " seconds" << endl;
+
+    return 0;
 }
